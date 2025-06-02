a133e7769629373703d9537c83222a08e2d6531a
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-06-02 13:09:12 -0700

    jank

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,11 @@ const CR_SPEED = 40
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _process(_delta: float):
+	#these functions control animations
 	if Input.is_action_pressed("crouch"):
-		_animated_sprite.play("Crouch")
-		_animated_sprite.pause()
+		_animated_sprite.play("Crouch start")
+	elif Input.is_action_just_released("crouch"):
+		_animated_sprite.play("Crouch end")
 	elif Input.is_action_pressed("left"):
 		_animated_sprite.play("Walk")
 	elif Input.is_action_pressed("right"):
@@ -19,7 +21,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -29,11 +30,9 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction * CR_SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, CR_SPEED)
-	
 	else:
 		if direction:
 			velocity.x = direction * SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-		
 	move_and_slide()
