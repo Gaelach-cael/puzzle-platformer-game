88119574ceda4d6be2ce2dfa2a9f4761879abde9
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-06-09 13:20:48 -0700

    THEMESS, THEMES for everyone

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready() var pause = preload("res://scenes/pause.tscn")
+@onready() var switch = preload("res://scenes/level 1/screen2.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,3 +12,5 @@ func _ready() -> void:
 func _process(_delta: float):
 	if Input.is_action_just_pressed("pause"):
 		get_parent().add_child(pause.instantiate())
+	if Input.is_action_pressed("switch"):
+		get_parent().add_child(switch.instantiate())
